cf56ac7b0c60c1f705ca436210a961863a7f42e8
gayhub <gayhub@live.cn>
2018-06-19 17:46:14 +0800

    curator

--- src/test/java/com/whh/spring/boot/controller/RedisTest.java
+++ src/test/java/com/whh/spring/boot/controller/RedisTest.java
@@ -4,6 +4,8 @@ import com.whh.spring.boot.BaseTest;
 import com.whh.spring.boot.dao.CmUser;
 import org.junit.Test;
 import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 import javax.annotation.Resource;
 
@@ -17,6 +19,9 @@ public class RedisTest extends BaseTest {
 
     @Resource
     private HashOperations<String, String, CmUser> hashOperations;
+
+    @Resource
+    private RedisTemplate redisTemplate;
 
     @Test
     public void test() {
@@ -32,5 +37,17 @@ public class RedisTest extends BaseTest {
         System.out.println("start :" + start);
         System.out.println("end :" + end);
         System.out.println("耗时：" + (end-start));
+    }
+
+    @Test
+    public void redisTemplateTest() {
+        CmUser cmUser = new CmUser();
+        cmUser.setAge(10);
+        cmUser.setId(10002L);
+        cmUser.setName("李四");
+        redisTemplate.opsForHash().put("USER_KEY", cmUser.getId(), cmUser);
+        CmUser cmUser1 = (CmUser) redisTemplate.opsForHash().get("USER_KEY", cmUser.getId());
+        //case 异常
+        System.out.println(cmUser1);
     }
 }
